b8b84d60ab7673505149f6a3c437dd07d3865458
yyx <775289630@qq.com>
2022-12-16 17:32:52 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 module.exports = defineConfig({
+  devServer: {
+    historyApiFallback: true,
+    allowedHosts: "all"
+  },
   transpileDependencies: true,
   configureWebpack: {
     plugins: [
